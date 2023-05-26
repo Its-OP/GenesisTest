13c3181a84223de5ec75d2ff636944e6cda6fb8e
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-05-26 22:46:47 +0300

    Implement EmailService

--- infrastructure/SendGridEmailClient.go
+++ infrastructure/SendGridEmailClient.go
@@ -1,6 +1,12 @@
 package infrastructure
 
-import "os"
+import (
+	"fmt"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	"log"
+	"os"
+)
 
 type SendGridEmailClient struct {
 	apiKey string
@@ -11,4 +17,32 @@ func NewSendGridEmailClient() *SendGridEmailClient {
 	client := &SendGridEmailClient{apiKey: apiKey}
 
 	return client
+}
+
+func (emailClient *SendGridEmailClient) Send(recipients []string, htmlContent string) {
+	if len(recipients) == 0 {
+		return
+	}
+
+	from := mail.NewEmail("Genesis School Applicant", "genesis.applicant@example.com")
+	firstTo := mail.NewEmail("Rate Recipient", recipients[0])
+	subject := "Current BTC to UAH rate"
+	message := mail.NewSingleEmail(from, subject, firstTo, "", htmlContent)
+
+	for i := 1; i < len(recipients); i++ {
+		personalization := mail.NewPersonalization()
+		personalization.AddTos(mail.NewEmail("Rate Recipient", recipients[i]))
+		message.AddPersonalizations(personalization)
+	}
+
+	client := sendgrid.NewSendClient(os.Getenv(emailClient.apiKey))
+
+	response, err := client.Send(message)
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
 }
