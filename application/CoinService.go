de3679a2603b43428908b762613d1cc4d0a4ddfd
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-05-27 17:46:39 +0300

    Implement SendEmails

--- application/CoinService.go
+++ application/CoinService.go
@@ -1,16 +1,20 @@
 package application
 
-import "btcRate/domain"
+import (
+	"btcRate/domain"
+	"fmt"
+)
 
 type CoinService struct {
 	coinClient      domain.ICoinClient
+	emailClient     domain.IEmailClient
 	emailRepository domain.IEmailRepository
 	currency        string
 	coin            string
 }
 
-func NewCoinService(client domain.ICoinClient, emailRepository domain.IEmailRepository, currency string) *CoinService {
-	service := &CoinService{coinClient: client, emailRepository: emailRepository, currency: currency}
+func NewCoinService(client domain.ICoinClient, emailClient domain.IEmailClient, emailRepository domain.IEmailRepository, currency string) *CoinService {
+	service := &CoinService{coinClient: client, emailClient: emailClient, emailRepository: emailRepository, currency: currency}
 
 	return service
 }
@@ -28,4 +32,16 @@ func (coinService *CoinService) GetCurrentRate(currency string, coin string) *do
 func (coinService *CoinService) Subscribe(email string) {
 	coinService.emailRepository.AddEmail(email)
 	coinService.emailRepository.Save()
+}
+
+func (coinService *CoinService) SendEmails() {
+	emails := coinService.emailRepository.GetAll()
+
+	currentPrice := coinService.GetCurrentRate(coinService.currency, coinService.coin)
+	htmlTemplate := `<p><strong>Amount:</strong> %f</p>
+	<p><strong>Currency:</strong> %s<p>
+	<p><strong>Timestamp:</strong> %s<p>`
+	htmlBody := fmt.Sprintf(htmlTemplate, currentPrice.Amount, currentPrice.Currency, currentPrice.Timestamp)
+
+	coinService.emailClient.Send(emails, htmlBody)
 }
